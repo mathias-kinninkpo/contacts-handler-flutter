2f57220c89dc0d1fb9381e7c38c0c6ff0f33402f
mathias-kinninkpo <kinninkpomathias@gmail.com>
2023-10-29 00:10:49 +0100

    Modification

--- lib/detail_page.dart
+++ lib/detail_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'sqlite/contact.dart';
 
 
 class SecondPage extends StatefulWidget {
-  const SecondPage({Key? key}) : super(key: key);
+  final int contactId;
+  const SecondPage({required this.contactId});
 
 
 
@@ -12,15 +14,24 @@ class SecondPage extends StatefulWidget {
 
 class _SecondPageState extends State<SecondPage> {
 
-  String _firstname = "";
-  String _lasttname = "";
-  String _profession = "";
+  String _fullName = "";
+  String _phone = "";
+  ContactProvider _databaseService = new ContactProvider();
+
+  _loadContactData() async {
+    Contact contact = await _databaseService.getContact(widget.contactId);
+    setState(() {
+      _fullName = contact.fullName ?? "";
+      _phone = contact.phone ?? "";
+    });
+  }
 
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    WidgetsBinding.instance!.addPostFrameCallback((_) =>  _loadContactData());
 
   }
   @override
@@ -31,21 +42,61 @@ class _SecondPageState extends State<SecondPage> {
         backgroundColor: Colors.green,
         centerTitle: true,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Center(
-            child: Column(
-              children: [
-                Text("Nom : $_lasttname \n"),
-                Text("Prénom : $_firstname \n"),
-                Text("Profession : $_profession \n"),
-              ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey,
+                    offset: Offset(0, 0),
+                    blurRadius: 1,
+                  ),
+                ],
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    "Détails du contact",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ListTile(
+                    leading: Icon(Icons.person, size: 48),
+                    title: Text(
+                      _fullName,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.phone, size: 48),
+                    title: Text(
+                      _phone,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          )
-        ],
+          ],
+        ),
       ),
+
     );
   }
 }
